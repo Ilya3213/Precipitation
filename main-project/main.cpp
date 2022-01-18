0854ed3c82e2953cf81e2b41c9439ed75f1fe5fe
Ilya3213 <97957773+Ilya3213@users.noreply.github.com>
2022-01-18 15:17:31 +0300

    Добавлен вывод общей информации о задании

--- main-project/main.cpp
+++ main-project/main.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
 
+using namespace std;
+
 int main()
 {
-    std::cout << "Hello\n";
+    cout << "Laboratory work #8. GIT\n";
+    cout << "Variant #3. Precipitation\n";
+    cout << "Author: Ilya Vezhik\n";
     return 0;
 }
